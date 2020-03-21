bfa2b2ae19d557238086a748adeeb89cb6089038
pradanahafiidh <pradanahafidh@gmail.com>
2020-03-21 20:10:45 +0700

    Tugas Lab 3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxiapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            //pengaturan nilai properties
+            taxi.DriverName = "Hafidh";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxiapp
+{
+    class Taxi
+    {
+        public string DriverName;
+        public bool OnDuty;
+        public int NumPassenger;
+        string status;
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : " + DriverName);
+            if (OnDuty == true)
+            {
+                status = "Yes";
+            }
+            else
+            {
+                status = "No";
+            }
+            Console.WriteLine("On Duty : " + status);
+            Console.WriteLine("Number of Passenger : " + NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(DriverName + " sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(DriverName + " selesai mengantar penumpamg");
+        }
+    }
+}
